f98f45ab4ddad4dcfbf619e0541bdc883e24a799
bjc <bjc@octo.com>
2018-09-28 00:10:15 +0200

    package et requirements
    This reverts commit b96d423

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ from setuptools import setup, Extension, find_packages
 setup(
     name='pybbl',
     version='1.0.0.dev0',
+    install_requires=['colored', 'pandas', 'numpy'],
+    packages=find_packages(),
 )
