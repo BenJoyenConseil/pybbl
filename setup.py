38defb7ed9b520c9b53d101b189ade3a26b088de
bjc <bjc@octo.com>
2018-09-28 00:11:10 +0200

    distribution d'une extension C
    This reverts commit 8069b69

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, Extension, find_packages
 
+module_a_compiler = Extension('iamanextension',
+                              sources = ['iamanextension/extension.c'])
+
 setup(
     name='pybbl',
     version='1.0.0.dev0',
@@ -7,6 +10,7 @@ setup(
     packages=find_packages(),
     py_modules=['iamamodule'],
     scripts=['bin/iamascript'],
+    ext_modules = [module_a_compiler],
     include_package_data=True, # https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files
     package_data={
             '': ['data/*.csv'], # https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files
