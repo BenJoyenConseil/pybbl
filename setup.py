9ac32fe5155fc3af5d4d83c2181f72d258db78c1
bjc <bjc@octo.com>
2018-09-28 00:35:35 +0200

    setup.cfg réduit le code boilerplate

--- setup.py
+++ setup.py
@@ -4,15 +4,5 @@ module_a_compiler = Extension('iamanextension',
                               sources = ['iamanextension/extension.c'])
 
 setup(
-    name='pybbl',
-    version='1.0.0.dev0',
-    install_requires=['colored', 'pandas', 'numpy'],
-    packages=find_packages(),
-    py_modules=['iamamodule'],
-    scripts=['bin/iamascript'],
     ext_modules = [module_a_compiler],
-    include_package_data=True, # https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files
-    package_data={
-            '': ['data/*.csv'], # https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files
-        }
 )
